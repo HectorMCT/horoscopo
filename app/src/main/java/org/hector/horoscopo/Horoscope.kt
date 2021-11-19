2e59ddcf2ad3e4a129b4db5cd9295e7b20ab426f
Hector Manuel Chavez Troncoso <hectorchavezt@hotmail.com>
2021-11-18 23:59:35 -0600

    Add chinese animals

---
+++ app/src/main/java/org/hector/horoscopo/Horoscope.kt
@@ -0,0 +1,7 @@
+package org.hector.horoscopo
+
+import android.media.Image
+
+data class Horoscope(val name: String, var years: IntArray, val image: Image) {
+
+}
--- app/src/main/java/org/hector/horoscopo/HoroscopeActivity.kt
+++ app/src/main/java/org/hector/horoscopo/HoroscopeActivity.kt
@@ -1,11 +1,25 @@
 package org.hector.horoscopo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.hector.horoscopo.databinding.ActivityHoroscopeBinding
 
 class HoroscopeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHoroscopeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_horoscope)
+
+        binding = ActivityHoroscopeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val relativeLayout = binding.rootRL
+
+        val images = intArrayOf(R.drawable.rat, R.drawable.ox, R.drawable.tigre, R.drawable.rabbit,
+            R.drawable.dragon, R.drawable.snake, R.drawable.horse, R.drawable.goat, R.drawable.monkey,
+            R.drawable.rooster, R.drawable.dog, R.drawable.pig)
+
+        relativeLayout.setBackgroundResource(images[1])
     }
 }
